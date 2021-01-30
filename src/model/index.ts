dbdf4a12fc2ce4de3a2470a9311e3f1acb209e9b
tkqubo <tk.qubo@gmail.com>
2021-01-30 19:24:38 +0900

    fix module export

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,8 +1,7 @@
 'use strict';
 
+export * from './api-request';
 export * from './api-response';
 export * from './core';
 export * from './survey';
 export * from './responses';
-export {SortOrder} from './api-request';
-export {HttpMethod} from './api-request';
